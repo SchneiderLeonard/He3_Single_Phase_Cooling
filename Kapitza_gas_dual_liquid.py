af9723e90d0e0458ad8b090ad66d91435a0fc8a4
SchneiderLeonard <50997134+SchneiderLeonard@users.noreply.github.com>
2019-07-25 16:25:41 -0500

    added finished Kapitza
    Single phase cooling at vapour and liquid state. Also combined with dual phase. Including a diagram.

--- Kapitza_gas_dual_liquid.py
+++ Kapitza_gas_dual_liquid.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 13 10:28:56 2019
+Created on Mon Jul 22 11:14:41 2019
 
 @author: L.Schneider, J.W.Martin
 """
@@ -18,9 +18,6 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import array as arr
-
-# command line options
-from optparse import OptionParser
 
 from sys import platform
 if any([platform.startswith(os_name) for os_name in ['linux', 'darwin', 'freebsd']]):
@@ -259,24 +256,12 @@ calc(IDID,PROP,JOUT,JIN1,VALU1,JIN2,VALU2,NPRCIS,NUNITS)
 #
 #CALCULATIONS-----------------------------------------------------------------------------------------------------------------------------------------------------
 
-#_DIFFERENT CASES WE LOOKED INTO
-
+#GASEOUS STATE-----------------------------------------------------------------
+        
+#_DIFFERENT CASES WE CAN LOOK INTO
 # Parameter in standard case: d=6.0mm;  Tstart=3.2;  Tbath=1.6;  Tend=2.25;  mdot=1.1;  step=0.01;  p0=30000;   TWALL IS EQUIVALENT TO THE HE4 BATH
 
-
-diameter = 6.2 # mm
-Tstart = 5. # K 
-Tbath = 1.6 # K
-Tend = 2.24# K
-mdot = 1.1 # g/s
-step = 0.01 # m 
-p0 = 30000. # Pa
-
-ell = 0.0  # m; starting position
-
-deltax = 0.005 # m; thickness of wall
-k_cu = 384.1 # W/m-K
-sigma_k = 900 # W/K^4-m^2
+p0 = 25000. # Pa
 
 # temperature at saturation
 Tk = ctypes.c_double()
@@ -284,19 +269,43 @@ pres = ctypes.c_double(p0)
 TIPsatdll(Tk,pres)
 saturation_temperature=Tk.value
 
+diameter = 6. # mm
+Tstart = 4. # K 
+Tbath = 1.4 # K
+Tend = saturation_temperature # K; end of vapour phase cooling
+Tend2 = 1.5 # K; end of liquid phase cooling
+mdot = 0.5 # g/s
+step = 0.01 # m 
+
+x0 = 1.0 # quality starts as vapour
+x1 = 0.0 # quality ends as liquid
+x = x0 # starting quality
+
+ell = 0.0  # m; starting position
+
+deltax = 0.005 # m; thickness of wall
+k_cu = 384.1 # W/m-K
+sigma_k = 6.5*21 # W/K^4-m^2
+
 d_m = diameter/1000 # m
 mdot_m = mdot/1000 # kg/s
+
 
 Xprop = (ctypes.c_double*40)()
  
 elllist = []
 Tlist = []
-Blist = [] 
 Tboundarylist = []
 Tw_olist = []
 Tw_ilist = []
 Tw_i2list = []
 qlist = []
+
+h_clist = []
+h_klist = []
+thermalclist = []
+h_k2list = []
+h_efflist = []
 
 
 while Tstart > Tend:    
@@ -327,21 +336,144 @@ while Tstart > Tend:
     k_t = Xprop[27] # W/m-K; thermal conductivity He3; k_t = k_f
     h_c = k_t*Nu/d_m # W/m^2-K; heat transfer coefficient
     #boundary to Cu; using an approximation of h_k @ -> sigma_k
-    h_k = 4.*sigma_k*Tstart**3 # W/m^2-K
+    h_k = sigma_k*Tstart**3 # W/m^2-K
     #thermal capacity
-    thermc = k_cu/deltax # [W/m^2-K]
+    thermalc = k_cu/deltax # [W/m^2-K]
     #Cu to He4; using an approximation of h_k2 @ -> sigma_k
     k_t2 = 0.003077  #need a HeCalc function: =HeClac(26,0,"SV","0","T",Tbath,1)
     k_t2 = PROP[2][26]
-    h_k2 = 4.*sigma_k*Tbath**3 # W/m^2-K
+    h_k2 = sigma_k*Tbath**3 # W/m^2-K
+    h_eff = (2/h_c + 1/h_k + 1/thermalc + 1/h_k2)**(-1)
     
-    q = (Tstart - Tbath)/(2/h_c + 1/h_k + deltax/k_cu + 1/h_k2) # W/m^2 
+    q = (Tstart - Tbath)/(2/h_c + 1/h_k + 1/thermalc + 1/h_k2) # W/m^2 
     
     Tboundary = -q/h_c + Tstart
     Tw_o =  -2*q/h_c + Tstart
     Tw_i = -q/h_k + Tw_o 
     Tw_i2 = -q/(k_cu/deltax) + Tw_i
     dT_f = h*pi*d_m*(Tw_i-Tstart)/mdot_m/C_p*step 
+    dT_f = -q*pi*d_m/mdot_m/C_p*step
+    
+#REST PARAMETERS
+    elllist.append(ell)
+    Tlist.append(Tstart)
+    Tboundarylist.append(Tboundary)
+    Tw_olist.append(Tw_o)
+    Tw_ilist.append(Tw_i)
+    Tw_i2list.append(Tw_i2)
+    qlist.append(q)
+    
+    h_clist.append(h_c)
+    h_klist.append(h_k)
+    thermalclist.append(thermalc)
+    h_k2list.append(h_k2)
+    h_efflist.append(h_eff)
+    
+    print("{:10.4f} {:10.4f} {:10.4e} {:10.4f} {:10.4f} {:10.4f} {:10.4f} {:10.4f}".format(ell,Tstart,Tboundary,Tw_o,Tw_i,Tw_i2,dT_f,q,Pr))
+    
+    ell = ell + step
+    Tstart = Tstart + dT_f    
+    
+
+#DUAL PHASE: APPROXIMATED------------------------------------------------------
+
+Tstart = saturation_temperature #K
+
+XpropL = (ctypes.c_double*40)()
+XpropV = (ctypes.c_double*40)()
+SatXFunTdll(XpropL,XpropV,Tk)
+xlist = []
+dQdLlist = []
+
+while x > 0:
+
+    rho_V = XpropV[2] # kg/m^3
+    h_V = XpropV[5] # J/kg
+    rho_L = XpropL[2] # kg/m^3
+    h_L = XpropL[5] # J/kg
+    mu_L = XpropL[28] # Pa*s
+    Re_0 = 4 * (mdot/1000.)/(pi*(diameter/1000.)*mu_L)
+    Pr_L = XpropL[31]
+    Nu = 0.023 * Re_0**0.8 * Pr_L**(1./3.)*sqrt((1-x)+rho_L/rho_V*x)
+    k_L = XpropL[27] # W/(m*K)
+    hc = Nu*k_L/(diameter/1000.)  # W/(m^2*K)
+    
+    dQ_dot = hc*(pi*d_m)*(Tstart-Tboundary)*step # W
+    
+    dx = dQ_dot/(-(mdot/1000.)*(h_V-h_L))
+    dQ_dot_over_dL = dQ_dot/step # W/m
+    #print(ell,Tk.value,p0,x,rho_V,h_V,rho_L,h_L,mu_L,Re_0,Pr_L,Nu,k_L,hc,dQ_dot,dx,dQ_dot_over_dL)
+    #further parameters for later calculations
+    mu_V = XpropV[28] 
+
+
+    xlist.append(x)
+    elllist.append(ell)
+    Tlist.append(Tstart)
+    dQdLlist.append(dQ_dot_over_dL)
+    Tboundarylist.append(Tboundary)
+    Tw_olist.append(Tw_o)
+    Tw_ilist.append(Tw_i)
+    Tw_i2list.append(Tw_i2)
+    qlist.append(q)
+    
+    h_clist.append(h_c)
+    h_klist.append(h_k)
+    thermalclist.append(thermalc)
+    h_k2list.append(h_k2)
+    h_efflist.append(h_eff)
+    
+    x = x + dx
+    ell = ell + step
+
+
+#LIQUID PHASE------------------------------------------------------------------
+
+while Tstart > Tend2:    
+
+    #Def rho
+    T_f = ctypes.c_double(Tstart)
+    pres = ctypes.c_double(p0)    
+    dkgm3 = ctypes.c_double()
+    idid = ctypes.c_int()
+    
+    DFPTdll(dkgm3,idid,pres,T_f)
+    #print(dkgm3.value,idid.value)      
+    rho = ctypes.c_double(dkgm3.value)
+
+    #Calc rest of Parameters
+    Fundtdll(Xprop,dkgm3,T_f)
+    
+    C_p = Xprop[8]
+    mu = Xprop[28]
+    Re = 4*mdot_m/(pi*d_m)/mu
+    Pr = Xprop[31]
+    Nu = 0.023*Re**0.8*Pr**0.3
+    k_f = Xprop[27]
+    h = Nu*k_f/d_m
+    
+    #Def q, h_c, h_k, k/delt(x), h_k', Tboundary, Tw,o, Tw,i1, Tw,i2     
+    #He3 to boundary
+    k_t = Xprop[27] # W/m-K; thermal conductivity He3; k_t = k_f
+    h_c = k_t*Nu/d_m # W/m^2-K; heat transfer coefficient
+    #boundary to Cu; using an approximation of h_k @ -> sigma_k
+    h_k = sigma_k*Tstart**3 # W/m^2-K
+    #thermal capacity
+    thermalc = k_cu/deltax # [W/m^2-K]
+    #Cu to He4; using an approximation of h_k2 @ -> sigma_k
+    k_t2 = 0.003077  #need a HeCalc function: =HeClac(26,0,"SV","0","T",Tbath,1)
+    k_t2 = PROP[2][26]
+    h_k2 = sigma_k*Tbath**3 # W/m^2-K
+    h_eff = (2/h_c + 1/h_k + 1/thermalc + 1/h_k2)**(-1)
+    
+    q = (Tstart - Tbath)/(2/h_c + 1/h_k + 1/thermalc + 1/h_k2) # W/m^2 
+    
+    Tboundary = -q/h_c + Tstart
+    Tw_o =  -2*q/h_c + Tstart
+    Tw_i = -q/h_k + Tw_o 
+    Tw_i2 = -q/(k_cu/deltax) + Tw_i
+    #dT_f = h*pi*d_m*(Tboundary-Tstart)/mdot_m/C_p*step
+    dT_f = -q*pi*d_m/mdot_m/C_p*step 
 
 #REST PARAMETERS
     elllist.append(ell)
@@ -352,29 +484,59 @@ while Tstart > Tend:
     Tw_i2list.append(Tw_i2)
     qlist.append(q)
     
-    print("{:10.4f} {:10.4f} {:10.4e} {:10.4f} {:10.4f} {:10.4f} {:10.4f}".format(ell,Tstart,Tboundary,Tw_o,Tw_i,Tw_i2,dT_f,q))
- 
+    h_clist.append(h_c)
+    h_klist.append(h_k)
+    thermalclist.append(thermalc)
+    h_k2list.append(h_k2)
+    h_efflist.append(h_eff)
+    
+    print("{:10.4f} {:10.4f} {:10.4e} {:10.4f} {:10.4f} {:10.4f} {:10.4f} {:10.4f}".format(ell,Tstart,Tboundary,Tw_o,Tw_i,Tw_i2,dT_f,q,Pr))
+    
     ell = ell + step
     Tstart = Tstart + dT_f    
-    
+
+
 #Plot: T(l) plot: use the plot function
-#plt.rc('text',usetex=True)      #font for LaTeX
-#plt.rc('font',family='serif')   #font for LaTeX
-plt.plot(elllist,Tlist,label='T_f')
-#custom size of graph _=plt.plot(3,1)
+
 #z = np.polyfit(elllist, Tlist, 2)
 #p = np.poly1d(z)
-
 #plt.plot(elllist,p(elllist),"r--")
-plt.plot(elllist,Tboundarylist,label='T_boundary')
-plt.plot(elllist,Tw_olist,label='Tw_o')
-plt.plot(elllist,Tw_ilist,label='Tw_i')
-plt.plot(elllist,Tw_i2list,label='Tw_i2')
-plt.plot([elllist[0],elllist[-1]],[1.6,1.6],"--",label='T_bath')
-plt.plot([elllist[0],elllist[-1]],[saturation_temperature,saturation_temperature],"--",color="cyan",label='T_saturation')
+plt.figure(figsize=(10,5))
+plt.rc('legend', fontsize=13)
+plt.rc('font', size=13, family='serif')     #font for LaTeX
+plt.plot(elllist,Tlist,label="T$_f$")
+plt.plot(elllist,Tboundarylist,label="T$_{boundary}$")
+plt.plot(elllist,Tw_olist,label="T$_{w,o}$")
+plt.plot(elllist,Tw_ilist,label="T$_{w,i}$")
+plt.plot(elllist,Tw_i2list,label="T$_{w,i}$")
+plt.plot([elllist[0],elllist[-1]],[Tbath ,Tbath],"--",label='T$_{bath}$')
+plt.plot([elllist[0],elllist[-1]],[saturation_temperature,saturation_temperature],"--",color="cyan",label="T$_{saturation}$")
 #plt.title("$T=%.3f(\mathrm{K m}^{-2})L^2+(%.3f)(\mathrm{K m}^{-1})L+%.3f\mathrm{K}$"%(z[0],z[1],z[2]))
 plt.xlabel('L [m]')     
 plt.ylabel('T [K]')         
+plt.legend(framealpha=1, frameon=True);
+plt.show()
+#f1.savefig("NAME",bbox_inches='tight')
+    
+    
+
+plt.figure(figsize=(10,5))
+plt.rc('legend', fontsize=13)
+plt.rc('font', size=13, family='serif')     #font for LaTeX
+
+plt.plot(elllist,h_clist,label="h$_c$")
+plt.plot(elllist,h_klist,label="h$_k$")
+plt.plot(elllist,h_k2list,label="h$_k'$")
+#plt.plot(elllist,thermalclist,label="k$_{cu}/Delta(x)$")
+plt.plot(elllist,h_efflist,label="h$_{eff}$")
+
+# Display grid
+plt.grid(True, which="both")
+# Linear X axis, Logarithmic Y axis
+plt.semilogy(elllist, h_klist )
+
+plt.xlabel('L [m]')     
+plt.ylabel('Conductance [W/m$^2$ K]')         
 plt.legend(framealpha=1, frameon=True);
 plt.show()
 #f1.savefig("NAME",bbox_inches='tight')
